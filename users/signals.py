b742697c697e7287454cfe497cee1fca7ab1e08d
Unyime Etim <lordunyime@gmail.com>
2020-08-14 17:22:32 +0100

    added images to signals

--- users/signals.py
+++ users/signals.py
@@ -162,3 +162,37 @@ def send_message_on_confirmed_PH(sender, instance, **kwargs):
         bot.send_message(chat_id="@%s" % telegram_settings['channel_name'],
             text=message_html, parse_mode=telegram.ParseMode.HTML)
 post_save.connect(send_message_on_confirmed_PH, sender=HelpTable) 
+
+
+def send_message_on_PH_proof_submit(sender, instance, **kwargs):
+    """send welcome message to new members"""
+        
+    if instance.approval_status == "Approved" and instance.user_proof:
+        provider = instance.provider
+        receiver = instance.receiver
+
+        amount = instance.amount
+        profile = Customer.objects.get(username=provider)
+        first_name = profile.first_name.capitalize()
+        last_name = profile.last_name.capitalize()
+
+        receiver_profile = Customer.objects.get(username=receiver)
+        receiver_first_name = receiver_profile.first_name
+        receiver_last_name = receiver_profile.last_name
+
+        message_html = render_to_string('users/send_message_on_PH_proof_submit.html', {
+            'first_name':first_name,
+            'last_name':last_name,
+            'amount':amount,
+            'receiver_first_name':receiver_first_name,
+            'receiver_last_name':receiver_last_name
+
+        })
+        photo =  instance.user_proofURL
+        telegram_settings = settings.TELEGRAM
+        bot = telegram.Bot(token=telegram_settings['bot_token'])
+        
+        bot.send_message(chat_id="@%s" % telegram_settings['channel_name'],
+        text=message_html, parse_mode=telegram.ParseMode.HTML)
+        bot.sendPhoto(chat_id="@%s" % telegram_settings['channel_name'], photo=photo)
+post_save.connect(send_message_on_PH_proof_submit, sender=HelpTable)
